d9a2b72ade96a9a6e7faef350335c2112161c7bf
sosh64 <giegoques@gmail.com>
2025-09-21 03:41:40 +0200

    app.py

--- app.py
+++ app.py
@@ -311,6 +311,8 @@ def index():
 
     return render_template_string(html_template, output=output)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
